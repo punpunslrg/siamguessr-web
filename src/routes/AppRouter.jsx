a11719a95ce697eba1c78c72625c788e56c8ae0b
bossszzzy <bossasinate@gmail.com>
2025-07-24 15:28:39 +0700

    Lobby กำังจะระเบิด

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,11 +1,15 @@
-
-
 function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-
+        <Route path="/round" element={<GeoRound />} />
+        <Route path="/homepagefree" element={<HomePageFree />} />
+        <Route path="/subscription" element={<Subscription />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/Lobby" element={<Lobby />} />
+        <Route path="/join/:roomCode" element={<Lobby />} />
+        <Route path="gameplay" element={<Gameplay />} />
       </Route>
     </Routes>
   );

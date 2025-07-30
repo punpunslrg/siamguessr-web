7dedcceb8e9f283d5010edf245008f744f322e7b
macjedsada <jedsadakorn.mac@gmail.com>
2025-07-30 13:32:40 +0700

    update SingleScore.jsx

--- src/pages/RoundScore.jsx
+++ src/pages/RoundScore.jsx
@@ -55,7 +55,7 @@ function RoundScore() {
   const totalScore = actionGetTotalScore();
 
   return (
-    <div className="min-h-screen bg-gray-900 text-white flex flex-col items-center justify-center px-4 py-10">
+    <div className=" bg-secondary flex flex-col items-center justify-center px-4 py-10">
       <div className="flex justify-center w-full max-w-3xl mb-4">
         <h2 className="text-3xl font-bold">
           Round {currentRoundIndex + 1} of {room.rounds.length}
@@ -101,10 +101,7 @@ function RoundScore() {
         </p>
       </div>
 
-      <button
-        onClick={handleNext}
-        className="mt-8 px-8 py-3 bg-indigo-600 hover:bg-indigo-700 text-white font-semibold rounded-lg shadow-lg cursor-pointer transition duration-300 ease-in-out transform hover:scale-105 active:scale-95"
-      >
+      <button onClick={handleNext} className="btn-round">
         {gameState === "game-over" ? "VIEW FINAL RESULTS" : "START NEXT ROUND"}
       </button>
     </div>
